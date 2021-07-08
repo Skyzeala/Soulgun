62ca681116597cc35388fe8a74420108d0fa0e25
Jared <jared.collums@gmail.com>
2021-07-08 00:22:09 -0700

    Initial texture manager header file

---
+++ TextureManager.h
@@ -0,0 +1,44 @@
+/**
+ * Texture Manager for Soulgun
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+
+enum TextureID {
+  TX_PLAYER,
+  TX_ROBOT,
+  TX_HUMAN,
+  TX_BULLET,
+  TX_TOTAL
+};
+
+/**
+ * Manages textures in memory
+ * 
+ * A pre-defined set of textures can be loaded, unloaded, 
+ * and retrieved as well as dimension meta data
+ */
+class TextureManager {
+public:
+  TextureManager(void);
+  ~TextureManager(void);
+
+  SDL_Texture *getTexture(TextureID id);
+  SDL_Point getDimensions(TextureID id);
+
+private:
+  std::string paths[TX_TOTAL] = {
+      "assets/placeholder-player.png", 
+      "assets/placeholder-robot.png",
+      "assets/placeholder-human.png", 
+      "assets/placeholder-bullet.png"
+    };
+
+  SDL_Texture *textures[TX_TOTAL];
+  SDL_Point dimensions[TX_TOTAL];
+
+  SDL_Texture *load(TextureID id);
+  void unload(TextureID id);
+};
